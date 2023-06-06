ca0a17b0225359d13f35a0ab1c95b672f2f6227e
Keating <keating.dev@protonmail.com>
2023-06-06 11:42:44 -0400

    WIP

--- src/L1ERC20Bridge.sol
+++ src/L1ERC20Bridge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 // L1 transfer
 // https://github.com/wormhole-foundation/wormhole/blob/main/ethereum/contracts/bridge/Bridge.sol#L191
-// 
+//
 // L2 Process
 // https://github.com/wormhole-foundation/wormhole/blob/main/ethereum/contracts/bridge/Bridge.sol#L431
 //
@@ -12,32 +12,63 @@ pragma solidity ^0.8.0;
 // Test from optimism Goerli to optimism goerli
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
+// TODO import wormhole relayer
+import {IWormholeRelayer} from "wormhole/interfaces/relayer/IWormholeRelayer.sol"
 
 contract L1ERC20Bridge {
   IERC20 public immutable L1_TOKEN;
   address public immutable L2_TOKEN_ADDRESS;
+
   // trusted relayer contract on this chain
   IWormholeRelayer immutable relayer;
 
-  constructor(address l1TokenAddress, address l2TokenAddress) {
-		  L1_TOKEN = IERC20(l1TokenAddress);
-		  L2_TOKEN_ADDRESS = l2TokenAddress;
+  // Wormhole id for the target chain
+  uint16 public immutable targetChain;
+
+  // Contract address on the target chain. We can probably use
+  // create2 for this.
+  address public immutable targetContract;
+
+  constructor(
+    address l1TokenAddress,
+    address l2TokenAddress,
+    address _coreRelayer,
+    uint16 targetChain,
+    address targetContract
+  ) {
+    L1_TOKEN = IERC20(l1TokenAddress);
+    L2_TOKEN_ADDRESS = l2TokenAddress;
+    relayer = IWormholeRelayer(_coreRelayer);
+    targetChain = targetChain;
+    targetContract = targetContract;
   }
 
-  function deposit(uint256 amount) external {
+  function deposit(address account, uint256 amount, address refundAccount, uint16 refundChain)
+    external payable returns (uint64 sequence)
+  {
     // TODO keep track of deposit
     L1_TOKEN.transferFrom(msg.sender, address(this), amount);
-	// mint on L2
-	// 1. encode packed mint args
-	// 2. send message to relayer contract
-	// 3. On L2 token receive and mint
-	_l2Mint(msg.sender, amount);
+    // mint on L2
+    // 1. encode packed mint args
+    // 2. send message to relayer contract
+    // 3. On L2 token receive and mint
+    return _l2Mint(account, amount, refundAccount, refundChain);
   }
 
   // Can't really test because relayers not released yet
-  function _l2Mint(address account, uint256 amount) {
+  function _l2Mint(address account, uint256 amount, address refundAccount, uint16 refundChain) external payable returns (uint64 sequence) {
     bytes memory mintCalldata = abi.encode(account, amount);
 
+    // TODO: Random value invoked on the target chain
+    uint256 gasLimit = 500_000;
+
+    //calculate cost to deliver message
+    (uint256 deliveryCost,) = relayer.quoteEVMDeliveryPrice(targetChain, receiverValue, gasLimit);
+
+    // Receiver value is 0 because we aren't passing any value
+    // to the target contract.
+    return relayer.sendPayloadToEvm{value: deliveryCost}(
+      targetChain, targetContract, mintCalldata, 0, gasLimit, refundChain, refundAccount
+    );
   }
 }
--- src/L2ERC20.sol
+++ src/L2ERC20.sol
@@ -8,8 +8,21 @@ import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Vo
 
 contract L2ERC20 is ERC20Votes {
   // TODO this can only be called by the wormhole relayer.
-  function mint(address account, uint256 amount) external public {
-		  _mint(account, amount);
+  // function mint(address account, uint256 amount) external public {
+  //   	  _mint(account, amount);
+  // }
+
+  // Decode message and then call _mint
+    function receiveWormholeMessages(
+      bytes memory payload,
+      bytes[] memory additionalVaas,
+      bytes32 sourceAddress,
+      uint16 sourceChain,
+      bytes32 deliveryHash
+  ) public payable override {}
+
+  modifier onlyRelayerContract() {
+    require(msg.sender == WORMHOLE_RELAYER_ADDRESS, "msg.sender is not WormholeRelayer contract.");
+    _;
   }
 }
-
