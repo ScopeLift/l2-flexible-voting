1d67904dd22f523412f97102497c4ecfec519b5b
Keating <keating.dev@protonmail.com>
2023-06-09 21:28:36 -0400

    Have generic relayer

--- test/L1Contracts.sol
+++ test/L1Contracts.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.0;
 contract L1Contracts {
     address wormholeRelayer = 0xA3cF45939bD6260bcFe3D66bc73d60f19e49a8BB;
 	uint16 wormholeFujiId = 6;
+    // address wormholeRelayer =0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0;
+	// uint16 wormholeFujiId = 5;
 }
