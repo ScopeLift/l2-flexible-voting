0dc891a8e92509e1737b801a79dcdc941b865eb9
Keating <keating.dev@protonmail.com>
2023-06-15 10:34:08 -0400

    Cleanup deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,43 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-// Write multi network deploy script
-// Take a look at the contracts-periphery
-// Has a multi network deploy script
-//
-// Next steps after deploy
-//
-// Write a script to execute a test transaction, verify this works
-// Then cleanup implmentation, validation
-// and solving the address problem
-// use the initializer pattern, probably the ERC20, then deploy script
-// deploy one and then the other, production level
-//
-// No tests for now, handle mocks in a separate pr or issue
-
-// Pull Ed in early next week, few calls with Ed, second call would
-// probably handle L2 flexible voting.
 import {Script, stdJson} from "forge-std/Script.sol";
 import {L1ERC20Bridge} from "src/L1ERC20Bridge.sol";
 import {L2ERC20} from "src/L2ERC20.sol";
-import {console2} from "forge-std/console2.sol";
-import {IERC20Receive} from "src/interfaces/IERC20Mint.sol";
+import {L1Contracts} from "test/L1Contracts.sol";
 
-contract Deploy is Script {
+/// @notice Deploy L1 bridge and corresponding token to be minted on L2
+contract Deploy is Script, L1Contracts {
   using stdJson for string;
 
   function run() public {
-    // Deploy the bridge
-    // then deploy the erc20Votes token
-    //
-    // Avalanche is mimicking the L1
-    address core = 0x7bbcE28e64B3F8b84d876Ab298393c38ad7aac4C;
     string memory file = "broadcast/DeployFakeERC20.s.sol/43113/run-latest.json";
     string memory json = vm.readFile(file);
     address deployedL1Token = json.readAddress(".transactions[0].contractAddress");
-
-    // Wormhole id for mumbai
-    uint16 targetChain = 5;
 
     setFallbackToDefaultRpcUrls(false);
 
@@ -45,12 +21,14 @@ contract Deploy is Script {
 
     vm.broadcast();
     L2ERC20 l2Token =
-      new L2ERC20("Scopeapotomus", "SCOPE", 0x0CBE91CF822c73C2315FB05100C2F714765d5c20);
+      new L2ERC20("Scopeapotomus", "SCOPE", wormholeCoreMumbai);
 
     vm.createSelectFork(getChain("avalanche_fuji").rpcUrl);
 
     vm.broadcast();
-    L1ERC20Bridge bridge = new L1ERC20Bridge(deployedL1Token, core);
+    L1ERC20Bridge bridge = new L1ERC20Bridge(deployedL1Token, wormholeCoreFuji);
+
+    vm.broadcast();
     bridge.initialize(address(l2Token));
   }
 }
