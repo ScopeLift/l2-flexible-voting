e0259e91983dae3683bb385be0e0655faa0687f0
Keating <keating.dev@protonmail.com>
2023-06-22 18:28:16 -0400

    Add natspec

--- src/FakeERC20.sol
+++ src/FakeERC20.sol
@@ -6,9 +6,13 @@ import {ERC20Permit} from "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 import {IERC20Mint} from "src/interfaces/IERC20Mint.sol";
 
+/// @notice An ERC20Votes token to help test the L2 voting system
 contract FakeERC20 is ERC20Votes, IERC20Mint {
   constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) ERC20Permit(_name) {}
 
+  /// @dev Mints tokens to address to help test bridging and voting.
+  /// @param account The address of where to mint the tokens.
+  /// @param amount The amount of tokens to mint.
   function mint(address account, uint256 amount) public {
     _mint(account, amount);
   }
--- test/L2ERC20.t.sol
+++ test/L2ERC20.t.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
-import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
-import {IERC20Mint} from "src/interfaces/IERC20Mint.sol";
 import {L2ERC20} from "src/L2ERC20.sol";
 import {Constants} from "test/Constants.sol";
 
-// TODO Add the most basic tests
-contract L2ERC20Test is Test {
+contract L2ERC20Test is Test, Constants {
   L2ERC20 erc20;
 
   function setUp() public {
