d2b48b8a278c4042cacf6bdcc7119b0bffff25f8
Keating <keating.dev@protonmail.com>
2023-07-05 12:59:18 -0400

    Add back example deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.16;
+
+import "forge-std/Script.sol";
+
+contract Deploy is Script {
+  Counter counter;
+
+  function run() public {}
+}
