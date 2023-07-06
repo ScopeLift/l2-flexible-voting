37151dba0f625a7d65472c18f8dda73704ae5736
Alexander Keating <keating.dev@protonmail.com>
2023-07-06 17:43:39 -0400

    Wormhole ERC20Votes token bridging implementation

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - [About](#about)
 - [Architecture](#architecture)
+- [Testing](#testing)
 - [License](#license)
 
 
@@ -22,8 +23,35 @@ Our initial architecture is shown in the diagram below. This provides a high lev
   <img width="900" src="./img/l2_flexible_voting_diagram.png" alt="Initial L2 Diagram">
 </div>
 
+
+## Testing
+
+To currently test the bridge add a wallet private key that has some native tokens on avalanche fuji
+and polygon mumbai. Then run the below docker commands to setup the spy and local redis node.
+
+```
+docker run --rm -p6379:6379 --name redis-docker -d redis 
+
+
+docker run \
+    --platform=linux/amd64 \
+    -p 7073:7073 \
+    --entrypoint /guardiand \
+    ghcr.io/wormhole-foundation/guardiand:latest \
+spy --nodeKey /node.key --spyRPC "[::]:7073" --network /wormhole/testnet/2/1 --bootstrap
+ /dns4/wormhole-testnet-v2-bootstrap.certus.one/udp/8999/quic/p2p/12D3KooWAkB9ynDur1Jtoa
+97LBUp8RXdhzS5uHgAfdTquJbrbN7i
+
+```
+
+Once those are running you will need to setup the L2 relayer. Follow the instructions 
+[here](https://github.com/ScopeLift/l2-wormhole-relayer-engine). Now, you should be able to run 
+the `MintOnL2` script demonstrating passing a  message cross-chain.
+
 ## License
 
 This project is available under the [MIT](LICENSE.txt) license.
 
 Copyright (c) 2023 ScopeLift
+
+
---
+++ src/L1ERC20Bridge.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20Votes} from "openzeppelin/token/ERC20/extensions/ERC20Votes.sol";
+import {IWormhole} from "wormhole/interfaces/IWormhole.sol";
+
+contract L1ERC20Bridge {
+  /// @notice L1 token used for deposits and voting.
+  ERC20Votes public immutable L1_TOKEN;
+
+  /// @notice Token address which is minted on L2.
+  address public L2_TOKEN_ADDRESS;
+
+  /// @notice The core Wormhole contract used to send messages to L2.
+  IWormhole immutable CORE_BRIDGE;
+
+  /// @notice A unique number used to send messages.
+  uint32 public nonce;
+
+  /// @notice Used to indicate whether the contract has been intialized with the L2 token address.
+  bool public INITIALIZED = false;
+
+  /// @dev Contract is already initialized with an L2 token.
+  error AlreadyInitialized();
+
+  /// @param l1TokenAddress The address of the L1 token.
+  /// @param _core The address of the core wormhole contract.
+  constructor(address l1TokenAddress, address _core) {
+    L1_TOKEN = ERC20Votes(l1TokenAddress);
+    CORE_BRIDGE = IWormhole(_core);
+    nonce = 0;
+  }
+
+  /// @notice Must be called before bridging tokens to L2.
+  /// @param l2TokenAddress The address of the L2 token.
+  function initialize(address l2TokenAddress) public {
+    if (INITIALIZED) revert AlreadyInitialized();
+    INITIALIZED = true;
+    L2_TOKEN_ADDRESS = l2TokenAddress;
+  }
+
+  /// @notice Deposits L1 tokens into bridge and publishes a message using Wormhole to the L2 token.
+  /// @param account The address of the user on L2 where to mint the token.
+  /// @param amount The amount of tokens to deposit and mint on the L2.
+  /// @return sequence An identifier for the message published to L2.
+  function deposit(address account, uint256 amount) external payable returns (uint64 sequence) {
+    L1_TOKEN.transferFrom(msg.sender, address(this), amount);
+
+    // TODO optimize with encodePacked
+    bytes memory mintCalldata = abi.encode(account, amount);
+    sequence = CORE_BRIDGE.publishMessage(nonce, mintCalldata, 1);
+    nonce = nonce + 1;
+    return sequence;
+  }
+}
