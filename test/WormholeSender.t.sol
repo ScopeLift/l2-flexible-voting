2419ac2db9413c9d4e724ec778f7412fb2dd2bdf
keating <keating.dev@protonmail.com>
2023-08-12 15:26:15 -0400

    Add tests for sender

---
+++ test/WormholeSender.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {IWormholeRelayer} from "wormhole/interfaces/relayer/IWormholeRelayer.sol";
+
+import {WormholeSender} from "src/WormholeSender.sol";
+import {Constants} from "test/Constants.sol";
+
+contract WormholeSenderHarness is WormholeSender {
+  constructor(address _relayer, uint16 _sourceChain, uint16 _targetChain)
+    WormholeSender(_relayer, _sourceChain, _targetChain)
+  {}
+
+  function wormholeRelayer() public view returns (IWormholeRelayer) {
+    return WORMHOLE_RELAYER;
+  }
+}
+
+contract WormholeSenderTest is Test, Constants {
+  WormholeSender wormholeSender;
+
+  function setUp() public {
+    vm.createSelectFork(L1_CHAIN.rpcUrl);
+
+    wormholeSender =
+    new WormholeSender(L1_CHAIN.wormholeRelayer, L1_CHAIN.wormholeChainId, L2_CHAIN.wormholeChainId);
+  }
+}
+
+contract Constructor is WormholeSenderTest {
+  function testForkFuzz_CorrectlySetsAllArgs(
+    address _wormholeRelayer,
+    uint16 _sourceChain,
+    uint16 _targetChain
+  ) public {
+    WormholeSenderHarness newSender =
+      new WormholeSenderHarness(_wormholeRelayer, _sourceChain, _targetChain);
+
+    assertEq(
+      address(newSender.wormholeRelayer()),
+      _wormholeRelayer,
+      "Wormhole relayer is not set correctly"
+    );
+    assertEq(newSender.SOURCE_CHAIN(), _sourceChain, "Source chain is not correctly set");
+    assertEq(newSender.TARGET_CHAIN(), _targetChain, "Target chain is not correctly set");
+  }
+}
+
+contract QuoteDeliveryCost is WormholeSenderTest {
+  function testFork_QuoteForDeliveryCostReturned() public {
+    uint256 cost = wormholeSender.quoteDeliveryCost(L1_CHAIN.wormholeChainId);
+    assertGt(cost, 0, "No cost was quoted");
+  }
+}
