165e503186e3748ec195858fcb3972d1021c0610
Ben DiFrancesco <ben@scopelift.co>
2023-11-08 11:05:59 -0500

    Assorted small comment & test cleanups

--- test/mock/L1BlockMock.sol
+++ test/mock/L1BlockMock.sol
@@ -2,20 +2,18 @@
 pragma solidity ^0.8.0;
 
 import {IL1Block} from "src/interfaces/IL1Block.sol";
-import {StdUtils} from "forge-std/Test.sol";
+import {TestConstants} from "test/Constants.sol";
 
 /// @dev For use in proposal related tests that need access to the L1Block only for the purpose of
 /// calculating an L2 block to emit in the proposal created events, for the sake of compatibility
 /// with existing frontend clients.
-contract L1BlockMock is IL1Block, StdUtils {
-  // An arbitrary, large, mainnet-block-like number for use with the mock L1 Block
-  uint64 private NUMBER = 18_442_511;
+contract L1BlockMock is IL1Block, TestConstants {
   uint64 private TIMESTAMP = 0;
   uint256 private BASEFEE = 0;
-  bytes32 private HASH = blockhash(NUMBER);
+  bytes32 private HASH = blockhash(MOCK_L1_BLOCK);
 
-  function number() external view returns (uint64) {
-    return NUMBER;
+  function number() external pure returns (uint64) {
+    return MOCK_L1_BLOCK;
   }
 
   function timestamp() external view returns (uint64) {
@@ -31,19 +29,19 @@ contract L1BlockMock is IL1Block, StdUtils {
   }
 
   /// @dev For use in tests to ensure a fuzzed L1 block vote end conforms to the internal invariant
-  /// requirements inside the L2GovernorMetadata
+  /// requirements inside the L2GovernorMetadata.
   function __boundL1VoteEnd(uint256 _l1VoteEnd) public view returns (uint256) {
-    return bound(_l1VoteEnd, NUMBER + 3000, NUMBER + 2_628_000);
+    return bound(_l1VoteEnd, MOCK_L1_BLOCK + 3000, MOCK_L1_BLOCK + 2_628_000);
   }
 
-  /// @dev Matches the implementation inside L2GovernorMetadata for the sake of test expectations
-  function __expectedL2BlockForFutureBlock(uint256 _l1BlockNumber) external view returns (uint256) {
+  /// @dev Matches the implementation inside L2GovernorMetadata for the sake of test expectations.
+  function __expectedL2BlockForFutureBlock(uint256 _l1BlockNumber) external pure returns (uint256) {
     require(
-      _l1BlockNumber > NUMBER + 1200,
+      _l1BlockNumber > MOCK_L1_BLOCK + 1200,
       "L1BlockMock: Bad test parameters, _l1BlockNumber must be greater than mock current block number"
     );
 
-    uint256 _l1BlocksUntilEnd = _l1BlockNumber - NUMBER - 1200;
+    uint256 _l1BlocksUntilEnd = _l1BlockNumber - MOCK_L1_BLOCK - 1200;
     return (_l1BlocksUntilEnd * 12) / 2;
   }
 }
